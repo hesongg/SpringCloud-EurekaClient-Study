07b19e77eab4d5047186e4a31655c8dcf11ec82a
hesongg <hsjo6864@gmail.com>
2023-01-13 16:28:56 +0900

    AuthenticationFilter 추가

--- src/main/java/study/springcloud/userservice/security/WebSecurity.java
+++ src/main/java/study/springcloud/userservice/security/WebSecurity.java
@@ -1,24 +1,47 @@
 package study.springcloud.userservice.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class WebSecurity {
     //WebSecurityConfigurerAdapter 가 deprecated 되었으므로 강의와 다른 방식으로 작업
 
+    private final AuthenticationConfiguration authenticationConfiguration;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeRequests().antMatchers("users/**").permitAll();
+//        http.authorizeRequests().antMatchers("/users/**").permitAll(); // 모든 요청 허용
+        http.authorizeRequests().antMatchers("/**")
+                .hasIpAddress("210.210.213.227")
+                .and()
+                .addFilter(getAuthenticationFilter());
 
         http.headers().frameOptions().disable();
 
         return http.build();
     }
+
+    private AuthenticationFilter getAuthenticationFilter() throws Exception {
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter();
+        authenticationFilter.setAuthenticationManager(authenticationManager());
+
+        return authenticationFilter;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager() throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
 
 }
